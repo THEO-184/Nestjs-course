b65678d55366cf7fe2859046b014f9768a0d0ecc
Theophilus Boakye Yiadom <65987461+THEO-184@users.noreply.github.com>
2023-07-22 22:22:25 +0000

    handle signup error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as bcrypt from 'bcrypt';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -13,14 +14,24 @@ export class AuthService {
   async signup(dto: AuthDto) {
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(dto.password, salt);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        // duplicating unique field
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taking');
+        }
+      }
 
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
-    delete user.hash;
-    return user;
+      throw error;
+    }
   }
 }
