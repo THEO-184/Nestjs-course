3397c044cb9f5299c71417241cbcae69ddacbda2
Theophilus Boakye Yiadom <65987461+THEO-184@users.noreply.github.com>
2023-07-23 20:55:33 +0000

    sign im

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as bcrypt from 'bcrypt';
@@ -7,9 +11,6 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  async signin() {
-    return { msg: 'I have signed in' };
-  }
 
   async signup(dto: AuthDto) {
     const salt = await bcrypt.genSalt(10);
@@ -28,6 +29,34 @@ export class AuthService {
         // duplicating unique field
         if (error.code === 'P2002') {
           throw new ForbiddenException('Credentials taking');
+        }
+      }
+
+      throw error;
+    }
+  }
+
+  async signin(dto: AuthDto) {
+    try {
+      const user = await this.prisma.user.findUniqueOrThrow({
+        where: {
+          email: dto.email,
+        },
+      });
+
+      const passwordMatches = await bcrypt.compare(dto.password, user.hash);
+
+      if (!passwordMatches) {
+        throw new NotFoundException('wrong password');
+        return;
+      }
+
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2025') {
+          throw new NotFoundException('wrong credentials');
         }
       }
 
